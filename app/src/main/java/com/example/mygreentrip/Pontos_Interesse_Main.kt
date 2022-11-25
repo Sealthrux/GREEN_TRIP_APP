c7fb2dd7c41878fa19f446f82987dd306b54f2c4
Just Seal <44335975+Sealthrux@users.noreply.github.com>
2022-11-25 10:41:39 +0000

    2

---
+++ app/src/main/java/com/example/mygreentrip/Pontos_Interesse_Main.kt
@@ -0,0 +1,11 @@
+package com.example.mygreentrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pontos_Interesse_Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pontos_interesse_main)
+    }
+}
