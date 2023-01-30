f72aaec5221a70ec0ec28c782b1c48e2c5cc4ac3
Stelly19 <nuno19092002@gmail.com>
2023-01-30 18:41:27 +0000

    perfil_alterar

---
+++ app/src/main/java/com/example/mygreentrip/Perfil_Alterar_Main.kt
@@ -0,0 +1,11 @@
+package com.example.mygreentrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Perfil_Alterar_Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_alterar_main)
+    }
+}
